1650aa693cc8b8932cd1072792b591ee162fc032
Alex <77325667+spookier@users.noreply.github.com>
2022-02-18 09:09:19 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <io.h>
+
+void rot13(char *string)
+{
+  int i = 0;
+
+  while (string[i] != '\0')
+  {
+    if (string[i] == ' ' || string[i] == '\t')
+    {
+      write(1, &string[i], 1);
+      i++;
+    }
+    if (string[i] >= 'A' && string[i] <= 'M')
+    {
+      string[i] += 13;
+      write(1, &string[i], 1);
+      i++;
+    }
+    if (string[i] >= 'N' && string[i] <= 'Z')
+    {
+      string[i] -= 13;
+      write(1, &string[i], 1);
+      i++;
+    }
+    if (string[i] >= 'a' && string[i] <= 'm')
+    {
+      string[i] += 13;
+      write(1, &string[i], 1);
+      i++;
+    }
+    if (string[i] >= 'n' && string[i] <= 'z')
+    {
+      string[i] -= 13;
+      write(1, &string[i], 1);
+      i++;
+    }
+    else
+    {
+      write(1, &string[i], 1);
+      i++;
+    }
+  }
+}
